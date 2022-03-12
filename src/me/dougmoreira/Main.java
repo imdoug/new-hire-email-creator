fe24e5020d0451cb027747a6e41884a97ad0d9b3
Doug Moreira <67474419+imdoug@users.noreply.github.com>
2022-03-12 01:08:37 -0500

    update

--- src/me/dougmoreira/Main.java
+++ src/me/dougmoreira/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         // DUMMY DATA FOR TESTING
         //  Email em1 = new Email("Douglas", "Moreira");
 
